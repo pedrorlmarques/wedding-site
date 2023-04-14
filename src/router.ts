c3d44c7a136dad738966fa1e17d1e09a5dcdc208
Pedro Cruz <pedropcruz@outlook.pt>
2023-04-14 01:17:12 +0100

    fix(): Invitation form on save guest and user info

--- src/router.ts
+++ src/router.ts
@@ -1,62 +1,64 @@
-import { createRouter, createWebHistory } from "vue-router";
-import { useAuth } from "@/composables/useAuth";
+import { createRouter, createWebHistory } from 'vue-router';
+import { useAuth } from '@/composables/useAuth';
 
 const routes = [
-  {
-    path: "/login",
-    name: "Login",
-    component: () => import("@/views/Login.vue"),
-  },
-  {
-    path: "/",
-    name: "Home",
-    meta: {
-      requiresAuth: true,
-    },
-    component: () => import("@/views/Home.vue"),
-  },
-  {
-    path: "/convite",
-    name: "Invitation",
-    meta: {
-      requiresAuth: true,
-    },
-    component: () => import("@/views/Invitation.vue"),
-  },
-  {
-    path: "/register",
-    name: "Register",
-    component: () => import("@/views/Register.vue"),
-  },
-  {
-    path: "/forgot-password",
-    name: "ForgotPassword",
-    component: () => import("@/views/ForgotPassword.vue"),
-  },
-  {
-    path: "/email-verification",
-    name: "EmailConfirmation",
-    component: () => import("@/views/EmailConfirmation.vue"),
-  },
-  {
-    path: "/recover-password",
-    name: "RecoverPassword",
-    component: () => import("@/views/RecoverPassword.vue"),
-  },
+	{
+		path: '/login',
+		name: 'Login',
+		component: () => import('@/views/Login.vue'),
+	},
+	{
+		path: '/',
+		name: 'Home',
+		meta: {
+			requiresAuth: true,
+		},
+		component: () => import('@/views/Home.vue'),
+	},
+	{
+		path: '/convite',
+		name: 'Invitation',
+		meta: {
+			requiresAuth: true,
+		},
+		component: () => import('@/views/Invitation.vue'),
+	},
+	{
+		path: '/register',
+		name: 'Register',
+		component: () => import('@/views/Register.vue'),
+	},
+	{
+		path: '/forgot-password',
+		name: 'ForgotPassword',
+		component: () => import('@/views/ForgotPassword.vue'),
+	},
+	{
+		path: '/email-verification',
+		name: 'EmailConfirmation',
+		component: () => import('@/views/EmailConfirmation.vue'),
+	},
+	{
+		path: '/recover-password',
+		name: 'RecoverPassword',
+		component: () => import('@/views/RecoverPassword.vue'),
+	},
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+	history: createWebHistory(),
+	routes,
 });
 
 router.beforeEach((to, _from, next) => {
-  const { isLoggedIn } = useAuth();
-  const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
+	const { isLoggedIn } = useAuth();
+	const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
 
-  if (requiresAuth && !isLoggedIn()) next("/login");
-  else if (!requiresAuth && isLoggedIn()) next("/");
-  else next();
+	console.log(!isLoggedIn());
+
+	if (requiresAuth && !isLoggedIn()) next('/login');
+	else if (!requiresAuth && isLoggedIn()) next('/');
+	else next();
 });
 
 export default router;
