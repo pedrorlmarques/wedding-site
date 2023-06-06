5e6cf8183dfaaef635c84cb7d4290f1a72369f18
Pedro Cruz <pedropcruz@outlook.pt>
2023-06-07 00:42:13 +0100

    fix(EmbeddedPlaylist): change texts and some styles on heading

--- src/components/Playlist/EmbeddedPlaylist/index.vue
+++ src/components/Playlist/EmbeddedPlaylist/index.vue
@@ -1,32 +1,31 @@
 <template>
-  <div>
-    <h2>Vamos para lá Fiesta?</h2>
-    <p>
-      Agora que já sabemos que vais, sabes prefeitamente que nós gostamos de
-      "fiesta", e deves saber que temos um gosto musical muito... peculiar.
-    </p>
-    <p>
-      Mas claro, nós queremos que este dia corre tudo ás mil maravilhas, e
-      queremos então que nos sugiras músicas!
-    </p>
-    <p>
-      Para isso, basta pesquisares na barra de pesquisa por qualquer música ou
-      até mesmo artista, seleccionas a música, e nós iremos depois decidir se
-      faz sentido a música ou não. Não queremos qualquer música!
-    </p>
-    <iframe
-      src="https://open.spotify.com/embed/playlist/1QLF09hp1OzJDc8EDNKWF4?utm_source=generator&theme=0"
-      width="100%"
-      height="352"
-      frameBorder="0"
-      allowfullscreen=""
-      loading="lazy"
-    />
-  </div>
+	<div class="">
+		<h1 class="block mt-8 text-center">Vamos compilar A Playlist!</h1>
+		<p>
+			Esta playlist vai ajudar o nosso DJ a perceber o gosto musical dos nossos
+			convidados.
+		</p>
+		<p>
+			Qualquer música pode ser pesquisada e sugerida usando a barra de pesquisa.
+			As músicas sugeridas irão para aprovação dos noivos e, assim que
+			aprovadas, as mesmas serão adicionadas à playlist que poderão ver abaixo.
+		</p>
+		<p>
+			Sejam criativos e não tenham vergonha de mostrar o vosso gosto musical que
+			gostariam de ouvir no nosso casamento!
+		</p>
+		<iframe
+			src="https://open.spotify.com/embed/playlist/1qu9RwXDmgiOL38kiJXEyG?si=iIToep8dRVCb5HieHGHv4A"
+			width="100%"
+			height="352"
+			frameBorder="0"
+			allowfullscreen=""
+			loading="lazy" />
+	</div>
 </template>
 <script setup></script>
 <style lang="postcss">
-iframe {
-  @apply rounded-xl;
-}
+	iframe {
+		@apply rounded-xl;
+	}
 </style>
